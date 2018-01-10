e722355b48fd125b5f2fb106a100aae5cb1a7bf2
Bashton19 <35300968+Bashton19@users.noreply.github.com>
2018-01-10 17:01:14 +0000

    Canvas

--- js/script.js
+++ js/script.js
@@ -1,35 +1,128 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
- 
-var painting = document.getElementById('paint');
-var paint_style = getComputedStyle(painting);
-canvas.width = parseInt(paint_style.getPropertyValue('width'));
-canvas.height = parseInt(paint_style.getPropertyValue('height'));
+if (window.addEventListener) {
+	window.addEventListener('load', function() { init(); });
+}
 
-var mouse = {x: 0, y: 0};
- 
-canvas.addEventListener('mousemove', function(e) {
-  mouse.x = e.pageX - this.offsetLeft;
-  mouse.y = e.pageY - this.offsetTop;
-}, false);
+var started = false;
+var canvas, context;
+var stampId = '';
+var lastColor = 'black';
+var lastStampId = '';
 
-ctx.lineWidth = 3;
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.strokeStyle = '#00CC99';
- 
-canvas.addEventListener('mousedown', function(e) {
-    ctx.beginPath();
-    ctx.moveTo(mouse.x, mouse.y);
- 
-    canvas.addEventListener('mousemove', onPaint, false);
-}, false);
- 
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
-}, false);
- 
-var onPaint = function() {
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
-};
+function init() {
+	canvas = $('#imageView').get(0);
+	context = canvas.getContext('2d');
+	
+	// Auto-adjust canvas size to fit window.
+	canvas.width  = window.innerWidth - 75;
+	canvas.height = window.innerHeight - 75;
+  
+	//$('#container').get(0).addEventListener('mousemove', onMouseMove, false);
+	canvas.addEventListener('mousemove', onMouseMove, false);
+	canvas.addEventListener('click', onClick, false);
+	
+	// Add events for toolbar buttons.
+	$('#red').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#pink').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#fuchsia').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#orange').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#yellow').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#lime').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#green').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#blue').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#purple').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#black').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#white').get(0).addEventListener('click', function(e) { onColorClick(e.target.id); }, false);
+	$('#cat').get(0).addEventListener('click', function(e) { onStamp(e.target.id); }, false);
+	$('#dragonfly').get(0).addEventListener('click', function(e) { onStamp(e.target.id); }, false);
+	$('#ladybug').get(0).addEventListener('click', function(e) { onStamp(e.target.id); }, false);
+	$('#heart').get(0).addEventListener('click', function(e) { onStamp(e.target.id); }, false);
+	$('#dog').get(0).addEventListener('click', function(e) { onStamp(e.target.id); }, false);
+	$('#fill').get(0).addEventListener('click', function(e) { onFill(); }, false);
+	$('#save').get(0).addEventListener('click', function(e) { onSave(); }, false);
+}
+
+function onMouseMove(ev) {
+	var x, y;
+		
+	// Get the mouse position.
+	if (ev.layerX >= 0) {
+		// Firefox
+		x = ev.layerX - 50;
+		y = ev.layerY - 5;
+	}
+	else if (ev.offsetX >= 0) {
+		// Opera
+		x = ev.offsetX - 50;
+		y = ev.offsetY - 5;
+	}
+	
+	if (!started) {
+		started = true;
+
+		context.beginPath();
+		context.moveTo(x, y);		
+	}
+	else {
+		context.lineTo(x, y);
+		context.stroke();
+	}
+	
+	$('#stats').text(x + ', ' + y);
+}
+
+function onClick(e) {
+	if (stampId.length > 0) {
+		context.drawImage($(stampId).get(0), e.pageX - 90, e.pageY - 60, 80, 80);
+	}
+}
+
+function onColorClick(color) {
+	// Start a new path to begin drawing in a new color.
+	context.closePath();
+	context.beginPath();
+	
+	// Select the new color.
+	context.strokeStyle = color;
+	
+	// Highlight selected color.
+	var borderColor = 'white';
+	if (color == 'white' || color == 'yellow') {
+		borderColor = 'black';
+	}
+	
+	$('#' + lastColor).css("border", "0px dashed white");
+	$('#' + color).css("border", "1px dashed " + borderColor);
+	
+	// Store color so we can un-highlight it next time around.
+	lastColor = color;
+}
+
+function onFill() {
+	// Start a new path to begin drawing in a new color.
+	context.closePath();
+	context.beginPath();
+
+	context.fillStyle = context.strokeStyle;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function onStamp(id) {
+	// Update the stamp image.
+	stampId = '#' + id;
+
+    if (lastStampId == stampId) {
+        // User clicked the selected stamp again, so deselect it.
+        stampId = '';
+    }
+
+	$(lastStampId).css("border", "0px dashed white");
+	$(stampId).css("border", "1px dashed black");
+	
+	// Store stamp so we can un-highlight it next time around.
+	lastStampId = stampId;	
+}
+
+function onSave() {
+	var img = canvas.toDataURL("image/png");
+	document.write('<img src="' + img + '"/>');
+}
