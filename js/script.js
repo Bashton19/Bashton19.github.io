35ea3a274c2b5925506568bcc0fd3994aa24eecf
Bashton19 <35300968+Bashton19@users.noreply.github.com>
2018-01-10 14:59:34 +0000

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+window.onload = function() {
+    init();
+    myMap();
+}
+function init(){
+    console.log('init fired');
+}
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
